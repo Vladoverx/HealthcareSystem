d912f12630d264c876be52e1f0d3c7b859b9eb59
Vladislav Bugaichenko <bug.vlad67@gmail.com>
2023-12-10 15:51:26 +0200

    Added AppointmentContext

---
+++ DAL/EF/AppointmentContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using DAL.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF
+{
+    public class AppointmentContext : DbContext
+    {
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Schedule> Scchedules { get; set; }
+
+        public AppointmentContext(DbContextOptions options) : base(options)
+        {
+        }
+
+  
+    }
+}
